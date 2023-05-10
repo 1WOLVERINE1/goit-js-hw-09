98ab4c23465ddf02eb9e4110110e1f0b281b9cc9
Ostap Huryk <ostap.modelka@gmail.com>
2023-05-11 01:06:20 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const bodyRef = document.querySelector('body');
+const btnStartRef = document.querySelector('button[data-start]');
+const btnStopRef = document.querySelector('button[data-stop]');
+let intervalId = null;
 
+btnStartRef.addEventListener('click', clickOnStart);
+function clickOnStart() {
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+btnStopRef.addEventListener('click', clickOnStop);
+function clickOnStop() {
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
